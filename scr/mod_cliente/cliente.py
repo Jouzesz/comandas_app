9156ffe149008d1519e46176a8adc54c049d0bf0
José <josemarceloandradealmeidajosemarceloandradealmeida@uniplaclages.edu.br>
2023-10-30 21:11:56 -0300

    att 13/20

--- scr/mod_cliente/cliente.py
+++ scr/mod_cliente/cliente.py
@@ -1,9 +1,39 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+from settings import HEADERS_API, ENDPOINT_CLIENTE
+from funcoes import Funcoes
 
 bp_cliente = Blueprint('cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 ''' rotas dos formulários '''
 
-@bp_cliente.route('/')
-def formListaCliente():
-    return render_template('formListaCliente.html'), 200
+@bp_cliente.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        compra_fiado = request.form['compra_fiado']
+        dia_fiado = request.form['dia_fiado']
+        #senha = request.form['senha’]
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        # monta o JSON para envio a API
+        payload = {'id_cliente': id_cliente, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_CLIENTE, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        print(result) # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code) # 200
+
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+        
+        return render_template('formListaCliente.html', msg=result[0])
+    
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
--- scr/mod_produto/produto.py
+++ scr/mod_produto/produto.py
@@ -1,9 +1,29 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
+from settings import HEADERS_API, ENDPOINT_PRODUTO
+import base64
 
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos formulários '''
 
-@bp_produto.route('/')
-def formListaProduto():
-    return render_template('formListaProduto.html'), 200
+@bp_produto.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_produto = request.form['id_produto']
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        #foto = request.form['foto']
+        valor_unitario = request.form['valor_unitario']
+        # converte a foto em base64
+        foto = "data:" + request.files['foto'].content_type + ";base64," + str(base64.b64encode(request.files['foto'].read()), "utf-8")
+
+        # monta o JSON para envio a API
+        payload = {'id_produto': id_produto, 'nome': nome, 'descricao': descricao, 'foto': foto, 'valor_unitario': valor_unitario}
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(urlApiProdutos, headers=headers, json=payload)
+        result = response.json()
+        return render_template('formListaProduto.html', msg=result[0])
+    except Exception as e:
+        return render_template('formListaProduto.html', msgErro=e.args[0])
